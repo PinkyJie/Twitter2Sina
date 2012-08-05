ebf41d8d40ea95a5f850fc6f413481c9bc0859be
PinkyJie <pilixiaoxuanfeng@gmail.com>
2012-08-06 00:06:09 +0800

    update sina sdk for oauth2

--- twitter2sina.py
+++ twitter2sina.py
@@ -5,21 +5,18 @@ import urllib,re,sys
 from google.appengine.ext import webapp,db
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-sys.path.insert(0, 'weibopy.zip')
-sys.path.append('weibopy.zip/weibopy')
 sys.path.insert(0, 'tweepy.zip')
 sys.path.append('tweepy.zip/tweepy')
 
 import tweepy
-from weibopy.auth import OAuthHandler
-from weibopy.api import API
-from weibopy.error import WeibopError
+from weibo import APIClient, APIError
 
 # twitter and sina app key
-app_key = '3275848911'
-app_secret = 'a9e5b80ec14bcdafd19ac2d47173aa92'
-consumer_key = "D7JSMFuPyFRUIKLz0vKTw"
-consumer_secret = "OthracjKzuvRYbnWUyJRYeMnLELf7xxmSNmCv78qPnk"
+APP_KEY = ""  # your sina app key
+APP_SECRET = "" # your sina app secret
+S_CALLBACK = "" # http://your_gae_url/oauth
+CONSUMER_KEY = "D7JSMFuPyFRUIKLz0vKTw"
+CONSUMER_SECRET = "OthracjKzuvRYbnWUyJRYeMnLELf7xxmSNmCv78qPnk"
 
 # regular expressions
 re_username = re.compile(r'@([a-z|A-Z|0-9|_]+):?')
@@ -33,21 +30,20 @@ re_fetch_url = re.compile('<a href="(.*)">')
 
 html_src = """
 <html>
-<head><title>Tui2Lang-Home</title></head>
+<head><title>Twitter2Sina-Home</title></head>
 <body><center>
-<h1>Tui2Lang</h1>
+<h1>Twitter2Sina</h1>
 <h3>A new self-service Twitter2Sina Sync application</h3>
 <form action="/result" method="post">
 <table>
-<tr><td>Twitter name: </td><td><input type="text" name="t_name" /></td><td></td></tr>
+<tr><td>Twitter name: </td><td><input type="text" name="t_name" /></td></tr>
 <tr><td>Twitter Oauth PIN: </td><td><input type="text" name="t_pin" /></td><td><a href="%s" target="_blank">Get Twitter Oauth Pin</a></td></tr>
-<tr><td>Sina email: </td><td><input type="text" name="s_name" /></td><td></td></tr>
-<tr><td>Sina Oauth PIN: </td><td><input type="text" name="s_pin" /></td><td><a href="%s" target="_blank">Get Sina Oauth Pin</a></td></tr>
+<tr><td>Sina name: </td><td><input type="text" name="s_name" value="%s" /></td></tr>
 </table>
 <input type="hidden" name="t_request_key" value="%s"><br>
 <input type="hidden" name="t_request_secret" value="%s"><br>
-<input type="hidden" name="s_request_key" value="%s"><br>
-<input type="hidden" name="s_request_secret" value="%s"><br>
+<input type="hidden" name="s_access_token" value="%s"><br>
+<input type="hidden" name="s_expire" value="%s"><br>
 <input type="submit" value="Submit">
 </center></body>
 </html>
@@ -88,40 +84,50 @@ def replace_tweet(tweet):
 class OauthUser(db.Model):
     twitter_name = db.StringProperty()
     sina_name = db.StringProperty()
-    sina_access_key = db.StringProperty()
-    sina_access_secret = db.StringProperty()
+    sina_access_token = db.StringProperty()
+    sina_expire = db.StringProperty()
     twitter_access_key = db.StringProperty()
     twitter_access_secret = db.StringProperty()
     twitter_last_id = db.StringProperty()
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        sina = OAuthHandler(app_key,app_secret)
-        sina_auth_url = sina.get_authorization_url()
-        twitter = tweepy.OAuthHandler(consumer_key,consumer_secret)
-        twitter_auth_url = twitter.get_authorization_url()
-        self.response.out.write(html_src % (twitter_auth_url,sina_auth_url,twitter.request_token.key,twitter.request_token.secret,sina.request_token.key,sina.request_token.secret))
+        sina = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=S_CALLBACK)
+        sina_auth_url = sina.get_authorize_url()
+        self.response.out.write("""<a href="%s">Sina Oauth</a>""" % sina_auth_url)
+        
+        
+class Oauth(webapp.RequestHandler):
+    def get(self):
+        s_pin = self.request.get('code')
+        if s_pin:
+            sina = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=S_CALLBACK)
+            r = sina.request_access_token(s_pin)
+            sina.set_access_token(r.access_token,r.expires_in)
+            twitter = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+            twitter_auth_url = twitter.get_authorization_url()
+            self.response.out.write(html_src % (twitter_auth_url,sina.users__show(uid=r.uid).screen_name,twitter.request_token.key,twitter.request_token.secret,r.access_token,r.expires_in))
+        else:
+            self.response.out.write("""<p>Error!!!<a href="/">Home</a></p>""")
+        
 
 class FormHandler(webapp.RequestHandler):
     def post(self):
         t_name = self.request.get('t_name')
         s_name = self.request.get('s_name')
-        s_request_key = self.request.get('s_request_key')
-        s_request_secret = self.request.get('s_request_secret')
+        s_access_token = self.request.get('s_access_token')
+        s_expire = self.request.get('s_expire')
         t_request_key = self.request.get('t_request_key')
         t_request_secret = self.request.get('t_request_secret')
         t_pin = self.request.get('t_pin')
-        s_pin = self.request.get('s_pin')
-        self.response.out.write("""<html><head><title>Tui2Lang-Result</title></head><body><center>""")
-        if t_name == "" or s_name =="" or s_pin == "":
+        self.response.out.write("""<html><head><title>Twitter2Sina-Result</title></head><body><center>""")
+        if t_name == "" or s_name =="":
             self.response.out.write("""<h2>4 Input can not be empty! <a href="/">Back</a></h2>""")
         else:
-            sina = OAuthHandler(app_key,app_secret)
-            sina.set_request_token(s_request_key,s_request_secret)
-            s_access_token = sina.get_access_token(s_pin.strip())
-            sina_api = API(sina)
-
-            twitter = tweepy.OAuthHandler(consumer_key,consumer_secret)
+            sina_api = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=S_CALLBACK)
+            sina_api.set_access_token(s_access_token,int(s_expire))
+            
+            twitter = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
             twitter.set_request_token(t_request_key,t_request_secret)
             t_access_token = twitter.get_access_token(t_pin.strip())
             twitter_api = tweepy.API(twitter);
@@ -129,24 +135,23 @@ class FormHandler(webapp.RequestHandler):
             t_tl = twitter_api.user_timeline()
             t_last_id = t_tl[0].id_str
             t_last_text = replace_tweet(t_tl[0].text)
-            
 
-            oauth_user = OauthUser(key_name=s_name)
+            oauth_user = OauthUser(key_name=t_name)
             oauth_user.twitter_name = t_name
             oauth_user.sina_name = s_name
-            oauth_user.sina_access_key = s_access_token.key
-            oauth_user.sina_access_secret = s_access_token.secret
+            oauth_user.sina_access_token = s_access_token
+            oauth_user.sina_expire = s_expire
             oauth_user.twitter_access_key = t_access_token.key
             oauth_user.twitter_access_secret = t_access_token.secret
             oauth_user.twitter_last_id = t_last_id
             oauth_user.put()
 
             try:
-                sina_api.update_status(t_last_text)            
-            except WeibopError,e:
+                sina_api.post.statuses__update(status=t_last_text)            
+            except APIError,e:
                 self.response.out.write(e)
             else:
-                self.response.out.write('Your Tui2Lang settings are successfully done!<br>')
+                self.response.out.write('Your Twitter2Sina settings are successfully done!<br>')
                 self.response.out.write('The last tweet synchronized is below:<br>')
                 self.response.out.write('<b>%s</b>' % t_last_text)
         self.response.out.write('</center></body></html>')
@@ -174,9 +179,8 @@ class AutoSync(webapp.RequestHandler):
                         break
                 if len(tweets_to_be_post) > 0:
                     # rebuild sina api
-                    sina = OAuthHandler(app_key,app_secret)
-                    sina.set_access_token(result.sina_access_key,result.sina_access_secret)
-                    sina_api = API(sina)
+                    sina_api = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=S_CALLBACK)
+                    sina_api.set_access_token(result.s_access_token,int(result.s_expire))
                     
                     for tweet_obj in reversed(tweets_to_be_post):
                         user = OauthUser.get_by_key_name(result.sina_name)
@@ -187,17 +191,18 @@ class AutoSync(webapp.RequestHandler):
                         tweet = replace_tweet(cur_tweet)
 			self.response.out.write(tweet)
                         try:
-                            sina_api.update_status(tweet)
+                            sina_api.post.statuses__update(status=tweet)
                             user.twitter_last_id = cur_id
                             user.put()
                             self.response.out.write('同步成功！')
-                        except WeibopError,e:
+                        except APIError,e:
                             self.response.out.write(e)
                             self.response.out.write('<br>')
 
 
 application = webapp.WSGIApplication(
     [('/', MainPage),
+    ('/oauth', Oauth),
      ('/result', FormHandler),
      ('/cron_sync', AutoSync),
      ],
